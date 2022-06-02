6fc879ab15ef7ec147a4c3cfa407a61a4c59dfbb
anton_panizovich1 <anton_panizovich@tut.by>
2022-06-02 23:45:00 +0300

    03hw

---
+++ src/com/vivamostoles/homework03/service/AuthorLastNamesList.java
@@ -0,0 +1,7 @@
+package src.com.vivamostoles.homework03.service;
+
+public interface AuthorLastNamesList {
+    String [] lastNames = new String[]{"Servantes", "Lope de Vega", "Bykau", "Karatkevich", "Orwell", "Miller", 
+            "Hugo", "Kolas"};
+
+}
