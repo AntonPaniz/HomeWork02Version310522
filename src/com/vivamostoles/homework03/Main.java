31c8d5de015b35dd01a2a411234a43414e29d53a
anton panizovich <anton_panizovich@tut.by>
2022-06-06 13:45:47 +0300

    versionWork 06/06 13:45

--- src/com/vivamostoles/homework03/Main.java
+++ src/com/vivamostoles/homework03/Main.java
@@ -1,12 +1,15 @@
 package src.com.vivamostoles.homework03;
 
+import src.com.vivamostoles.homework03.service.LiteraryWorks;
 import src.com.vivamostoles.homework03.utils.BookGenerator;
 import src.com.vivamostoles.homework03.utils.BookGeneratorForHashSet;
 import src.com.vivamostoles.homework03.utils.RemoveBook;
 import src.com.vivamostoles.homework03.utils.UsingForToPrint;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,25 +18,36 @@ public class Main {
       System.out.println("After using remove");
     RemoveBook.removeByIndex(6, list);
        UsingForToPrint.usingFor(list);*/
-      HashSet <Book> list = BookGeneratorForHashSet.generation(2);
+      HashSet <Book> books = BookGeneratorForHashSet.generation(19);
+      Set<Character> letters = new HashSet<>(Arrays.asList('A', 'E', 'I', 'O', 'U', 'Y', 'a', 'e', 'i', 'o','u','y'));
       Book equalBook1 = new Book("Kupala", "Yanka", "Daminikavich", "Paulinka");
         Book equalBook2 = new Book("Kupal", "Yank", "Daminikavic", "Paulinka");
         Book equalBook3 = new Book("Kupa", "Yan", "Daminikavi", "Paulinka");
         Book equalBook4 = new Book("Kupa", "Yan", "Daminikav", "Paulinka");
         Book equalBook5 = new Book("Kup", "Ya", "Daminika", "Paulinka");
         Book equalBook6 = new Book("Ku", "Y", "Daminik", "Paulinka");
-      list.add(equalBook1);
-        list.add(equalBook2);
-        list.add(equalBook3);
-        list.add(equalBook4);
-        list.add(equalBook5);
-        list.add(equalBook6);
-        UsingForToPrint.usingForHashSet(list);
-        /*for (Book book : listOfBooksFromHashSet) {
-            System.out.println(listOfBooksFromHashSet);}*/
-        // System.out.println(listOfBooksFromHashSet);
+      books.add(equalBook1);
+        books.add(equalBook2);
+        books.add(equalBook3);
+        books.add(equalBook4);
+        books.add(equalBook5);
+        books.add(equalBook6);
+      //  UsingForToPrint.usingForHashSet(list);
+     // System.out.println(books.contains("e"));
+      for (Book book:books
+           ) {
+        String bookName = book.getLiteraryWork();
+        Character firstLetter = bookName.charAt(0);
+        if (letters.contains(firstLetter)) {
+          System.out.println(bookName);
+        }
+      }
 
-        /*HashSet <String> beatles = new HashSet<>(4);
+
+
+
+
+               /*HashSet <String> beatles = new HashSet<>(4);
         beatles.add("John");
         beatles.add("Paul");
         beatles.add("George");
--- src/com/vivamostoles/homework03/service/LiteraryWorks.java
+++ src/com/vivamostoles/homework03/service/LiteraryWorks.java
@@ -1,7 +1,7 @@
 package src.com.vivamostoles.homework03.service;
 
 public interface LiteraryWorks {
-    String [] literaryWorks = new String[]{"Don Quixote", "El perro del hortelano", "Miertvym nie balic", "Kalasy pad syarpom tvaim",
+    String [] literaryWorks = new String[]{"Don Quixote", "el perro del hortelano", "Ablava", "Kalasy pad syarpom tvaim",
             "Pinskaya shlyachta", "Black Spring", "Les Miserables", "Na rostannyach"};
 
 }
