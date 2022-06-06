eb70d97616933d6f052f4cb19954c501a288f15e
anton panizovich <anton_panizovich@tut.by>
2022-06-06 10:10:08 +0300

    versionWork 06/06 10:09

--- src/com/vivamostoles/homework03/Main.java
+++ src/com/vivamostoles/homework03/Main.java
@@ -1,17 +1,20 @@
 package src.com.vivamostoles.homework03;
 
 import src.com.vivamostoles.homework03.utils.BookGenerator;
+import src.com.vivamostoles.homework03.utils.BookGeneratorForHashSet;
 import src.com.vivamostoles.homework03.utils.RemoveBook;
 import src.com.vivamostoles.homework03.utils.UsingForToPrint;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List <Book> list = BookGenerator.generation(20);
+    /*    List <Book> list = BookGenerator.generation(20);
          UsingForToPrint.usingFor(list);
       System.out.println("After using remove");
     RemoveBook.removeByIndex(6, list);
-       UsingForToPrint.usingFor(list);
+       UsingForToPrint.usingFor(list);*/
+        HashSet <Book> listOfBooksFromHashSet = BookGeneratorForHashSet.generation(3);
     }}
 
---
+++ src/com/vivamostoles/homework03/utils/BookGeneratorForHashSet.java
@@ -0,0 +1,33 @@
+package src.com.vivamostoles.homework03.utils;
+
+import src.com.vivamostoles.homework03.Book;
+import src.com.vivamostoles.homework03.service.AuthorFirstNamesList;
+import src.com.vivamostoles.homework03.service.AuthorLastNamesList;
+import src.com.vivamostoles.homework03.service.LiteraryWorks;
+import src.com.vivamostoles.homework03.service.PatronymicNamesList;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+
+public class BookGeneratorForHashSet {
+    public static HashSet<Book> generation(int numberOfBooksToMake) {
+        HashSet<Book> books = new HashSet<>(numberOfBooksToMake);
+        String [] authorLastName = AuthorLastNamesList.lastNames;
+        String [] authorFirstName = AuthorFirstNamesList.firstNames;
+        String [] patronymicName = PatronymicNamesList.patronymicNames;
+        String [] literaryWork = LiteraryWorks.literaryWorks;
+        for (int i = 0; i <numberOfBooksToMake; i++){
+            int randomValueOfLastName = new Random().nextInt(authorLastName.length);
+            int randomValueOfFirstName = new Random().nextInt(authorFirstName.length);
+            int randomValueOfpatronymicName = new Random().nextInt(patronymicName.length);
+            int randomValueOfliteraryWork = new Random().nextInt(LiteraryWorks.literaryWorks.length);
+            Book bookFromGenerator = new Book(authorLastName[randomValueOfLastName],
+                    authorFirstName[randomValueOfFirstName], patronymicName[randomValueOfpatronymicName],
+                    literaryWork[randomValueOfliteraryWork]);
+            books.add(bookFromGenerator);
+            // System.out.println(books[i]);
+        }
+        return books;
+}}
