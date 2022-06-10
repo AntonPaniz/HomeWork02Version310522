032f86a3693d8360da6ce7091619892894e20ecc
anton panizovich <anton_panizovich@tut.by>
2022-06-10 14:02:27 +0300

    versionWork 10/06 14:02

--- src/com/vivamostoles/homework03/Main.java
+++ src/com/vivamostoles/homework03/Main.java
@@ -1,5 +1,7 @@
 package src.com.vivamostoles.homework03;
 
+import src.com.vivamostoles.homework03.service.AuthorFirstNamesList;
+import src.com.vivamostoles.homework03.service.AuthorLastNamesList;
 import src.com.vivamostoles.homework03.service.Bookfields;
 import src.com.vivamostoles.homework03.service.LiteraryWorks;
 import src.com.vivamostoles.homework03.utils.*;
@@ -8,59 +10,45 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-    TreeSet <String> students = TreeSetCreator.generation(7);
-      for (String stude : students){
-        System.out.println("after creat  " + stude); }
-      TreeSetCreator.deleteStudent(students);
-        /*for (String stude : students){
-            System.out.println("after remove  " + stude); }*/
-
-     // System.out.println(students);
-
- /*    List <Book> list = BookGenerator.generation(3);
+      //Solution part 01 of the task
+      List <Book> list = BookGenerator.generation(20);
       UsingForToPrint.usingFor(list);
-     // System.out.println("After using remove");
-     //  RemoveBook.removeByIndex(6, list);
-       //UsingForToPrint.usingFor(list);*/
-        HashSet<Book> books = (HashSet<Book>) BookGeneratorForHashSet.generation(3, 1);
-       // Set<Character> letters = new HashSet<>(Arrays.asList('A', 'E', 'I', 'O', 'U', 'Y', 'a', 'e', 'i', 'o', 'u', 'y'));
-        /*Book equalBook1 = new Book("Kupala", "Yanka", "Daminikavich", "Paulinka");
-        Book equalBook2 = new Book("Kupal", "Yank", "Daminikavic", "Paulinka");
-        Book equalBook3 = new Book("Kupa", "Yan", "Daminikavi", "Paulinka");
-        Book equalBook4 = new Book("Kupa", "Yan", "Daminikav", "Paulinka");
-        Book equalBook5 = new Book("Kup", "Ya", "Daminika", "Paulinka");
-        Book equalBook6 = new Book("Ku", "Y", "Daminik", "Paulinka");
-        books.add(equalBook1);
-        books.add(equalBook2);
-        books.add(equalBook3);
-        books.add(equalBook4);
-        books.add(equalBook5);
-        books.add(equalBook6);*/
-     /*   UsingForToPrint.usingForHashSet(books);
+      System.out.println("After using remove");
+      RemoveBook.removeByIndex(6, list);
+      UsingForToPrint.usingFor(list);
+
+
+      //Solution part 01 of the task
+       HashSet<Book> books = (HashSet<Book>) BookGeneratorForHashSet.generation(25, 6);
+       UsingForToPrint.usingForHashSet(books);
+       Set<Character> letters = new HashSet<>(Arrays.asList('A', 'E', 'I', 'O', 'U', 'Y', 'a', 'e', 'i', 'o', 'u', 'y'));
+
         for (Book book : books) {
             String bookName = book.getLiteraryWork();
             Character firstLetter = bookName.charAt(0);
             if (letters.contains(firstLetter)) {
-                System.out.println("task about letter>> " + bookName + " " + book.getAuthorLastName() + " " + book.getAuthorFirstName() + " " + book.getPatronymicName());
+                System.out.println("task about letter>> " + bookName + " "
+                        + book.getAuthorLastName() + " " + book.getAuthorFirstName() + " " + book.getPatronymicName());
             }
-        }*/
-        for (Book versionOfBooks: books)
-        {System.out.println("checking>>>" + versionOfBooks);}
+        }
+        UsingForToPrint.usingForHashSet(books);
+
+
+
         SortedBook.sortBooks(books, Bookfields.NAME);
         SortedBook.sortBooks(books,Bookfields.FIRSTNAME);
         SortedBook.sortBooks(books,Bookfields.PATRONUMIC);
         SortedBook.sortBooks(books, Bookfields.LITERARYWORK);
 
-     //From here Sergei version
-       /* Set <Book> bookSortedByFirstName = SortedBook.booksorting(books, "FirstName");
-        for (Book printbook: bookSortedByFirstName
-             ) {
-            System.out.println("Sorted books>>>>" + printbook);
 
-        };*/
-        //  List sortedList = new ArrayList(books);
-  /*  List <Book> sortedList = new ArrayList<Book>(books);
-      Collections.sort(sortedList);*/
+         BookGenerator.creatingRandomLine(4);
+         BookGenerator.getLineFromArray(AuthorLastNamesList.lastNames);
+
+
+        TreeSet <String> students = TreeSetCreator.generation(7);
+        for (String stude : students){
+            System.out.println("after creat  " + stude); }
+        TreeSetCreator.deleteStudent(students);
 
     }
 }
--- src/com/vivamostoles/homework03/utils/BookGenerator.java
+++ src/com/vivamostoles/homework03/utils/BookGenerator.java
@@ -1,10 +1,7 @@
 package src.com.vivamostoles.homework03.utils;
 
 import src.com.vivamostoles.homework03.Book;
-import src.com.vivamostoles.homework03.service.AuthorFirstNamesList;
-import src.com.vivamostoles.homework03.service.AuthorLastNamesList;
-import src.com.vivamostoles.homework03.service.LiteraryWorks;
-import src.com.vivamostoles.homework03.service.PatronymicNamesList;
+import src.com.vivamostoles.homework03.service.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,4 +28,25 @@ public class BookGenerator {
         return books;
 
     }
+    public static String creatingRandomLine(int length){
+        String resourceLine="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random=new Random();
+        StringBuffer intermediateValue =new StringBuffer();
+        for(int i=0;i<length;i++){
+            int number=random.nextInt(62);
+            intermediateValue.append(resourceLine.charAt(number));
+        }
+        System.out.println("random line from random line>>" + resourceLine);
+        return intermediateValue.toString();
+
+    }
+
+       public static String getLineFromArray(String [] lineToStick){
+
+            String arrayToString = String.join(" ", lineToStick);
+
+            System.out.println("random line from array >>" + arrayToString);
+          return  arrayToString;
+        }
+
 }
